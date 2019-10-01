a82c70e8f6fc49199da350575fad7d1a0e21be07
Luis Caraballo <luisred61@gmail.com>
2019-10-01 15:58:49 -0500

    Second, Third and Fourth Challenge completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+localStorage.clear();
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
@@ -21,11 +22,31 @@ const getData = api => {
       $app.appendChild(newItem);
       localStorage.setItem("next_fetch", response.info.next)
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(`No podemos conectarnos al API: Error ${error} `));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  let nextUrlPage = localStorage.getItem("next_fetch")
+  try {
+
+    if (nextUrlPage)
+      await getData(nextUrlPage)
+
+    else if (nextUrlPage === "") {
+      console.log("No Existe mas personajes")
+      let lastDiv = document.createElement("div");
+      lastDiv.classList.add("end");
+      lastDiv.innerHTML = `<h1>No Existen mas personajes</h1>`
+      $app.appendChild(lastDiv);
+      intersectionObserver.unobserve($observe)
+    }
+    else
+      await getData(API)
+
+  } catch (error) {
+      console.log (`Ha ocurrido un error al intentar mostrar la info`);
+  }
+
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
